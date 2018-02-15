9d7ad53f7305c8f56523c87e7ddc16d64245ae38
Christopher Hair <chris.hair@makcg.com>
2018-02-15 14:17:40 -0500

    adding in an API service for interfacing with UI

---
+++ src/server/DemoCluster.Api/DemoClusterApi.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Orleans.Providers;
+
+namespace DemoCluster.Api
+{
+    public class DemoClusterApi : IBootstrapProvider
+    {
+        private IWebHost host;
+
+        public string Name { get; private set; }
+
+        public Task Init(string name, IProviderRuntime providerRuntime, IProviderConfiguration config)
+        {
+            Name = name;
+
+            return Task.CompletedTask;
+        }
+
+        public Task Close()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
