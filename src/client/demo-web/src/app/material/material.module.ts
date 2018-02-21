f72c5c60b7a769e4f3a1414046edfd81040f7e72
Christopher Hair <chris.hair@makcg.com>
2018-02-21 17:03:32 -0500

    getting some of the inital setup done for the web app

---
+++ src/client/demo-web/src/app/material/material.module.ts
@@ -0,0 +1,70 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import {
+  MatSnackBarModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatGridListModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatSelectModule,
+  MatSlideToggleModule,
+  MatToolbarModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatIconModule,
+  MatSidenavModule,
+  MatListModule,
+  MatTabsModule,
+  MatTableModule,
+  MatSortModule,
+  MatPaginatorModule,
+  MatProgressSpinnerModule
+} from '@angular/material';
+
+@NgModule({
+  imports: [
+    MatSnackBarModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatGridListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatSlideToggleModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatTabsModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule
+  ],
+  exports: [
+    MatSnackBarModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatGridListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatSlideToggleModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatTabsModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule
+  ]
+})
+export class MaterialModule { }
