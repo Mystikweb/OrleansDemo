175e86f113d5f9dafc1436ab6d3824cadbe445cf
Christopher Hair <mystikweb@live.ca>
2018-05-19 14:38:01 -0400

    removing custom mongo storage

--- src/server/Orleans.Storage.Mongo/MongoConstants.cs
+++
@@ -1,9 +0,0 @@
-namespace Orleans.Storage.Mongo
-{
-    internal enum MongoProviderLogCode
-    {
-        ReadingRedisData = 10,
-        WritingRedisData = 20,
-        ClearingRedisData = 30
-    }
-}
