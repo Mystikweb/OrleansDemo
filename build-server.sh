860b05352f3a0d8decaaf2448b603e6c945a70d6
Christopher Hair <mystikweb@live.ca>
2018-08-10 16:46:57 -0400

    fixing build server

--- build-server.sh
+++ build-server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 dotnet restore ./src/DemoCluster.sln
 dotnet build ./src/DemoCluster.sln
-docker build -t mystikweb/orleans-demo-silo -f DockerCluster .
+docker build -t mystikweb/orleans-demo-silo:development -f DockerCluster .
+docker build -t mystikweb/orleans-demo-configuration:development -f DockerConfigApi .
