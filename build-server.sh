b0e0b004d9d5ee55f278fb8ad90cbc4df2e50cd7
Christopher Hair <mystikweb@live.ca>
2018-08-10 17:12:17 -0400

    adding docker push and slack updates to CI build

--- build-server.sh
+++ build-server.sh
@@ -2,4 +2,6 @@
 dotnet restore ./src/DemoCluster.sln
 dotnet build ./src/DemoCluster.sln
 docker build -t mystikweb/orleans-demo-silo:development -f DockerCluster .
+docker push mystikweb/orleans-demo-silo
 docker build -t mystikweb/orleans-demo-configuration:development -f DockerConfigApi .
+docker push mystikweb/orleans-demo-configuration
