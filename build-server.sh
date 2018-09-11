02b6aba29a232708c5e9f6b643a18b2ba9064db8
Christopher Hair <mystikweb@live.ca>
2018-09-11 09:04:05 -0400

    updating build not do the docker build right now

--- build-server.sh
+++ build-server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 dotnet restore ./src/DemoCluster.sln
 dotnet build ./src/DemoCluster.sln
-docker build -t mystikweb/orleans-demo-silo:development -f DockerCluster .
-docker push mystikweb/orleans-demo-silo
-docker build -t mystikweb/orleans-demo-configuration:development -f DockerConfigApi .
-docker push mystikweb/orleans-demo-configuration
+# docker build -t mystikweb/orleans-demo-silo:development -f DockerCluster .
+# docker push mystikweb/orleans-demo-silo
+# docker build -t mystikweb/orleans-demo-configuration:development -f DockerConfigApi .
+# docker push mystikweb/orleans-demo-configuration
