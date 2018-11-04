ad0e9e748dcbeb39e117cc4ca93983434b4f31b4
Christopher Hair <mystikweb@live.ca>
2018-11-04 18:30:09 -0500

    fixing string formatting for repository

--- build-server.sh
+++ build-server.sh
@@ -4,7 +4,7 @@ dotnet build ./src/DemoCluster.sln
 
 docker login ${REGISTRY_LOCATION} -u ${REGISTRY_USER} -p ${REGISTRY_PASSWORD}
 
-docker build -t ${TRAVIS_REPO_SLUG}Silo:${TRAVIS_BRANCH} -f DockerCluster .
-docker tag ${TRAVIS_REPO_SLUG}Silo:${TRAVIS_BRANCH} ${REGISTRY_LOCATION}/${TRAVIS_REPO_SLUG}Silo:${TRAVIS_BRANCH}
+docker build -t ${TRAVIS_REPO_SLUG,,}silo:${TRAVIS_BRANCH} -f DockerCluster .
+docker tag ${TRAVIS_REPO_SLUG,,}silo:${TRAVIS_BRANCH} ${REGISTRY_LOCATION}/${TRAVIS_REPO_SLUG,,}silo:${TRAVIS_BRANCH}
 # docker build -t mystikweb/orleans-demo-configuration:development -f DockerConfigApi .
 # docker push mystikweb/orleans-demo-configuration
