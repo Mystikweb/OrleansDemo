2321584983331d9da459dabc573ea25fc47a2342
Christopher Hair <mystikweb@live.ca>
2018-11-04 18:55:41 -0500

    adding repo push to registry

--- build-server.sh
+++ build-server.sh
@@ -6,5 +6,6 @@ docker login https://${REGISTRY_LOCATION} -u ${REGISTRY_USER} -p ${REGISTRY_PASS
 
 docker build -t ${TRAVIS_REPO_SLUG,,}silo:${TRAVIS_BRANCH} -f DockerCluster .
 docker tag ${TRAVIS_REPO_SLUG,,}silo:${TRAVIS_BRANCH} ${REGISTRY_LOCATION}/${TRAVIS_REPO_SLUG,,}silo:${TRAVIS_BRANCH}
+docker push ${REGISTRY_LOCATION}/${TRAVIS_REPO_SLUG,,}silo:${TRAVIS_BRANCH}
 # docker build -t mystikweb/orleans-demo-configuration:development -f DockerConfigApi .
 # docker push mystikweb/orleans-demo-configuration
