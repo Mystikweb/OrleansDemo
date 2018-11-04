7efaa5307fd8dbd4d0bbec03232f603730b1570c
Christopher Hair <mystikweb@live.ca>
2018-11-04 18:22:51 -0500

    build updates

--- build-server.sh
+++ build-server.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 dotnet restore ./src/DemoCluster.sln
 dotnet build ./src/DemoCluster.sln
-# docker build -t mystikweb/orleans-demo-silo:development -f DockerCluster .
-# docker push mystikweb/orleans-demo-silo
+
+docker login ${REGISTRY_LOCATION} -u ${REGISTRY_USER} -p ${REGISTRY_PASSWORD}
+
+docker build -t ${TRAVIS_REPO_SLUG}Silo:${TRAVIS_BRANCH} -f DockerCluster .
+docker tag ${TRAVIS_REPO_SLUG}Silo:${TRAVIS_BRANCH} ${REGISTRY_LOCATION}/${TRAVIS_REPO_SLUG}Silo:${TRAVIS_BRANCH}
 # docker build -t mystikweb/orleans-demo-configuration:development -f DockerConfigApi .
 # docker push mystikweb/orleans-demo-configuration
