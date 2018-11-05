835f3f8681232fcdb87fb570abbb7f38ec4b2fcc
Christopher Hair <mystikweb@live.ca>
2018-11-04 20:27:55 -0500

    testing verbosity in docker build

--- push-silo.sh
+++ push-silo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker login https://${REGISTRY_LOCATION}/v2 -u ${REGISTRY_USER} -p ${REGISTRY_PASSWORD}
+echo ${REGISTRY_PASSWORD} | docker login https://${REGISTRY_LOCATION}/v2 --username ${REGISTRY_USER} --password-stdin
 
 docker build -t ${TRAVIS_REPO_SLUG,,}silo:${TRAVIS_BRANCH} -f DockerCluster .
 docker tag ${TRAVIS_REPO_SLUG,,}silo:${TRAVIS_BRANCH} ${REGISTRY_LOCATION}/${TRAVIS_REPO_SLUG,,}silo:${TRAVIS_BRANCH}
