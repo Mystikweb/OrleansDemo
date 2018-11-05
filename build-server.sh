c59b2079d5d79db76665fe0f27af8d06206df47a
Christopher Hair <mystikweb@live.ca>
2018-11-04 19:02:54 -0500

    more tweaks to push docker image

--- build-server.sh
+++ build-server.sh
@@ -2,7 +2,7 @@
 dotnet restore ./src/DemoCluster.sln
 dotnet build ./src/DemoCluster.sln
 
-docker login https://${REGISTRY_LOCATION} -u ${REGISTRY_USER} -p ${REGISTRY_PASSWORD}
+docker login https://${REGISTRY_LOCATION}/v2 -u ${REGISTRY_USER} -p ${REGISTRY_PASSWORD}
 
 docker build -t ${TRAVIS_REPO_SLUG,,}silo:${TRAVIS_BRANCH} -f DockerCluster .
 docker tag ${TRAVIS_REPO_SLUG,,}silo:${TRAVIS_BRANCH} ${REGISTRY_LOCATION}/${TRAVIS_REPO_SLUG,,}silo:${TRAVIS_BRANCH}
