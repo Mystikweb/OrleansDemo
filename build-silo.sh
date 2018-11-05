3a193c268c89d02c8feffd84c2c401e426abdabe
Christopher Hair <mystikweb@live.ca>
2018-11-04 20:17:15 -0500

    minimizing verbosity on build

--- build-silo.sh
+++ build-silo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-dotnet build ./src/DemoCluster.sln
+dotnet build -v m ./src/DemoCluster.sln
 
 # docker build -t mystikweb/orleans-demo-configuration:development -f DockerConfigApi .
 # docker push mystikweb/orleans-demo-configuration
