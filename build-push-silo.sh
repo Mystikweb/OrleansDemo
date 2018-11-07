a4975ba268445c7fa4bb31f4b567f0f1774e3e30
Christopher Hair <mystikweb@live.ca>
2018-11-07 00:40:24 -0500

    adding checks for image tagging in CI scripts

--- build-push-silo.sh
+++ build-push-silo.sh
@@ -2,7 +2,10 @@
 
 echo "Creating staging folder for docker build of the silo"
 
+DOCKER_TAG="latest"
 STAGING_DIR="./silo-staging/"
+
+if [ ${TRAVIS_BRANCH} != "master"]; then let DOCKER_TAG=${TRAVIS_BRANCH}; fi
 
 if [ -d "$STAGING_DIR" ]; then rm -Rf "$STAGING_DIR"; fi
 
@@ -19,11 +22,11 @@ rsync src/DemoCluster.Silo/Dockerfile "$STAGING_DIR"
 
 echo "Files copied to $STAGING_DIR starting Docker build process"
 cd "$STAGING_DIR"
-docker build -t ${TRAVIS_REPO_SLUG,,}silo:${TRAVIS_BRANCH} .
+docker build -t ${TRAVIS_REPO_SLUG,,}silo:${DOCKER_TAG} .
 
 echo "Docker image built successfully pushing to $REGISTRY_LOCATION"
 
 echo ${REGISTRY_PASSWORD} | docker login https://${REGISTRY_LOCATION}/v2 --username ${REGISTRY_USER} --password-stdin
 
-docker tag ${TRAVIS_REPO_SLUG,,}silo:${TRAVIS_BRANCH} ${REGISTRY_LOCATION}/${TRAVIS_REPO_SLUG,,}silo:${TRAVIS_BRANCH}
-docker push ${REGISTRY_LOCATION}/${TRAVIS_REPO_SLUG,,}silo:${TRAVIS_BRANCH}
+docker tag ${TRAVIS_REPO_SLUG,,}silo:${DOCKER_TAG} ${REGISTRY_LOCATION}/${TRAVIS_REPO_SLUG,,}silo:${DOCKER_TAG}
+docker push ${REGISTRY_LOCATION}/${TRAVIS_REPO_SLUG,,}silo:${DOCKER_TAG}
