bef0f88d7745c38413ca801877857f188976259a
Christopher Hair <mystikweb@live.ca>
2018-11-07 01:03:56 -0500

    tag name fix

--- build-push-runtime.sh
+++ build-push-runtime.sh
@@ -5,7 +5,7 @@ echo "Creating staging folder for docker build of the runtime"
 DOCKER_TAG="latest"
 STAGING_DIR="./runtime-staging/"
 
-if [ "$TRAVIS_BRANCH" != "master" ]; then let DOCKER_TAG=${TRAVIS_BRANCH}; fi
+if [ "$TRAVIS_BRANCH" != "master" ]; then let DOCKER_TAG="$TRAVIS_BRANCH"; fi
 
 if [ -d "$STAGING_DIR" ]; then rm -Rf "$STAGING_DIR"; fi
 
@@ -20,11 +20,11 @@ rsync src/DemoCluster.Runtime/Dockerfile "$STAGING_DIR"
 
 echo "Files copied to $STAGING_DIR starting Docker build process"
 cd "$STAGING_DIR"
-docker build -t ${TRAVIS_REPO_SLUG,,}runtime:${DOCKER_TAG} .
+docker build -t ${TRAVIS_REPO_SLUG,,}runtime:"$DOCKER_TAG" .
 
 echo "Docker image built successfully pushing to $REGISTRY_LOCATION"
 
 echo ${REGISTRY_PASSWORD} | docker login https://${REGISTRY_LOCATION}/v2 --username ${REGISTRY_USER} --password-stdin
 
-docker tag ${TRAVIS_REPO_SLUG,,}runtime:${DOCKER_TAG} ${REGISTRY_LOCATION}/${TRAVIS_REPO_SLUG,,}runtime:${DOCKER_TAG}
+docker tag ${TRAVIS_REPO_SLUG,,}runtime:"$DOCKER_TAG" ${REGISTRY_LOCATION}/${TRAVIS_REPO_SLUG,,}runtime:"$DOCKER_TAG"
 docker push ${REGISTRY_LOCATION}/${TRAVIS_REPO_SLUG,,}runtime:${DOCKER_TAG}
