aee9a950c393facf6b74855a99e5d370cfb78b86
Christopher Hair <mystikweb@live.ca>
2018-11-06 23:32:26 -0500

    bash script fixes

--- build-push-runtime.sh
+++ build-push-runtime.sh
@@ -2,20 +2,20 @@
 
 echo "Creating staging folder for docker build of the runtime"
 
-STAGING_DIR = "runtime-staging"
+STAGING_DIR = ./runtime-staging
 
-if [ -d "$STAGING_DIR" ]; then rm -Rf $STAGING_DIR; fi
+if [ -d "$STAGING_DIR" ]; then rm -Rf "$STAGING_DIR"; fi
 
-mkdir -p $STAGING_DIR
+mkdir -p "$STAGING_DIR"
 
-rsync -av --exclude 'bin/' --exclude 'obj/' src/DemoCluster $STAGING_DIR
-rsync -av --exclude 'bin/' --exclude 'obj/' src/DemoCluster.DAL $STAGING_DIR
-rsync -av --exclude 'bin/' --exclude 'obj/' src/DemoCluster.Hosting $STAGING_DIR
-rsync -av --exclude 'bin/' --exclude 'obj/' --exclude 'Dockerfile' src/DemoCluster.Runtime $STAGING_DIR
-rsync src/DemoCluster.Runtime/Dockerfile $STAGING_DIR
+rsync -av --exclude 'bin/' --exclude 'obj/' src/DemoCluster "$STAGING_DIR"
+rsync -av --exclude 'bin/' --exclude 'obj/' src/DemoCluster.DAL "$STAGING_DIR"
+rsync -av --exclude 'bin/' --exclude 'obj/' src/DemoCluster.Hosting "$STAGING_DIR"
+rsync -av --exclude 'bin/' --exclude 'obj/' --exclude 'Dockerfile' src/DemoCluster.Runtime "$STAGING_DIR"
+rsync src/DemoCluster.Runtime/Dockerfile "$STAGING_DIR"
 
 echo "Files copied to $STAGING_DIR starting Docker build process"
-cd $STAGING_DIR
+cd "$STAGING_DIR"
 docker build -t ${TRAVIS_REPO_SLUG,,}runtime:${TRAVIS_BRANCH} .
 
 echo "Docker image built successfully pushing to $REGISTRY_LOCATION"
--- build-push-silo.sh
+++ build-push-silo.sh
@@ -2,23 +2,23 @@
 
 echo "Creating staging folder for docker build of the silo"
 
-STAGING_DIR = "silo-staging"
+STAGING_DIR = ./silo-staging
 
-if [ -d "$STAGING_DIR" ]; then rm -Rf $STAGING_DIR; fi
+if [ -d "$STAGING_DIR" ]; then rm -Rf "$STAGING_DIR"; fi
 
-mkdir -p $STAGING_DIR
+mkdir -p "$STAGING_DIR"
 
-rsync -av --exclude 'bin/' --exclude 'obj/' src/DemoCluster $STAGING_DIR
-rsync -av --exclude 'bin/' --exclude 'obj/' src/DemoCluster.DAL $STAGING_DIR
-rsync -av --exclude 'bin/' --exclude 'obj/' src/DemoCluster.GrainImplementations $STAGING_DIR
-rsync -av --exclude 'bin/' --exclude 'obj/' src/DemoCluster.GrainInterfaces $STAGING_DIR
-rsync -av --exclude 'bin/' --exclude 'obj/' src/DemoCluster.Hosting $STAGING_DIR
-rsync -av --exclude 'bin/' --exclude 'obj/' src/Orleans.Storage.Redis $STAGING_DIR
-rsync -av --exclude 'bin/' --exclude 'obj/' --exclude 'Dockerfile' src/DemoCluster.Silo $STAGING_DIR
-rsync src/DemoCluster.Silo/Dockerfile $STAGING_DIR
+rsync -av --exclude 'bin/' --exclude 'obj/' src/DemoCluster "$STAGING_DIR"
+rsync -av --exclude 'bin/' --exclude 'obj/' src/DemoCluster.DAL "$STAGING_DIR"
+rsync -av --exclude 'bin/' --exclude 'obj/' src/DemoCluster.GrainImplementations "$STAGING_DIR"
+rsync -av --exclude 'bin/' --exclude 'obj/' src/DemoCluster.GrainInterfaces "$STAGING_DIR"
+rsync -av --exclude 'bin/' --exclude 'obj/' src/DemoCluster.Hosting "$STAGING_DIR"
+rsync -av --exclude 'bin/' --exclude 'obj/' src/Orleans.Storage.Redis "$STAGING_DIR"
+rsync -av --exclude 'bin/' --exclude 'obj/' --exclude 'Dockerfile' src/DemoCluster.Silo "$STAGING_DIR"
+rsync src/DemoCluster.Silo/Dockerfile "$STAGING_DIR"
 
 echo "Files copied to $STAGING_DIR starting Docker build process"
-cd $STAGING_DIR
+cd "$STAGING_DIR"
 docker build -t ${TRAVIS_REPO_SLUG,,}silo:${TRAVIS_BRANCH} .
 
 echo "Docker image built successfully pushing to $REGISTRY_LOCATION"
